e9ddd8eacd6c404a00c20b9c9394ba1b456e932b
fuadmoin <fuadamoin@gmail.com>
2023-04-28 18:52:04 +0300

    Make required changes.

--- src/component/Home.js
+++ src/component/Home.js
@@ -10,18 +10,22 @@ import right from '../img/right.png';
 const Home = () => {
   const dispatch = useDispatch();
   const { animals, loading } = useSelector((state) => state.animal);
-  const category1 = animals.filter((animal) => animal.breeds.primary === 'Pug' || animal.breeds.primary === 'Terrier'
-    || animal.breeds.primary === 'Tabby' || animal.breeds.primary === 'Spaniel' || animal.breeds.primary === 'Domestic Long Hair');
-  const category2 = animals.filter((animal) => animal.breeds.primary === 'Domestic Short Hair' || animal.breeds.primary === 'Border'
-    || animal.breeds.primary === 'German Shorthaired Pointer' || animal.breeds.primary === 'Golden Retriever' || animal.breeds.primary === 'Australian Cattle Dog');
-  const category3 = animals.filter((animal) => animal.breeds.primary === 'Beagle' || animal.breeds.primary === 'Old English Sheepdog' || animal.breeds.primary === 'Mixed Breed'
-    || animal.breeds.primary === 'Doberman Pinscher' || animal.breeds.primary === 'Hound' || animal.breeds.primary === 'Border Collie');
-  const category4 = animals.filter((animal) => animal.breeds.primary === 'Labrador Retriever' || animal.breeds.primary === 'German Shepherd Dog'
-    || animal.breeds.primary === 'New Zealand' || animal.breeds.primary === 'Chihuahua' || animal.breeds.primary === 'Boxer' || animal.breeds.primary === 'Shih Tzu');
-  const category5 = animals.filter((animal) => animal.breeds.primary === 'Plott Hound' || animal.breeds.primary === 'Blue Heeler'
-    || animal.breeds.primary === 'Dachshund' || animal.breeds.primary === 'Pomeranian' || animal.breeds.primary === 'Domestic Medium Hair');
-  const category6 = animals.filter((animal) => animal.breeds.primary === 'Miniature Schnauzer' || animal.breeds.primary === 'Whippet'
-    || animal.breeds.primary === 'Rottweiler' || animal.breeds.primary === 'Persian' || animal.breeds.primary === 'Pit Bull Terrier' || animal.breeds.primary === 'Yorkshire Terrier');
+
+  const handleCategory = (category) => {
+    let result = [];
+    category.forEach((element) => {
+      const animal = animals.filter((animal) => animal.breeds.primary === element);
+      result = [...result, ...animal];
+    });
+    return result;
+  };
+
+  const category1 = handleCategory(['Pug', 'Terrier', 'Tabby', 'Spaniel', 'Domestic Long Hair']);
+  const category2 = handleCategory(['Domestic Short Hair', 'Border', 'German Shorthaired Pointer', 'Golden Retriever', 'Australian Cattle Dog']);
+  const category3 = handleCategory(['Beagle', 'Old English Sheepdog', 'Mixed Breed', 'Doberman Pinscher', 'Hound', 'Border Collie']);
+  const category4 = handleCategory(['Labrador Retriever', 'German Shepherd Dog', 'New Zealand', 'Chihuahua', 'Boxer', 'Shih Tzu']);
+  const category5 = handleCategory(['Plott Hound', 'Blue Heeler', 'Dachshund', 'Pomeranian', 'Domestic Medium Hair']);
+  const category6 = handleCategory(['Miniature Schnauzer', 'Whippet', 'Rottweiler', 'Persian', 'Pit Bull Terrier', 'Yorkshire Terrier']);
   useEffect(() => {
     if (!animals.length) { dispatch(fetchAnimals()); }
   }, [dispatch, animals]);
